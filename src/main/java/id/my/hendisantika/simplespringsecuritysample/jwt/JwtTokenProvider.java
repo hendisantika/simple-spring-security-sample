db1cb2e2b3d837b0c41a9d58504869b8bddff807
hendisantika <hendisantika@yahoo.co.id>
2024-10-08 06:11:21 +0700

    Add JwtTokenProvider🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/simplespringsecuritysample/jwt/JwtTokenProvider.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.simplespringsecuritysample.jwt;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : simple-spring-security-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/10/24
+ * Time: 06.10
+ * To change this template use File | Settings | File Templates.
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app-jwt-expiration-milliseconds}")
+    private long jwtExpirationDate;
+}
