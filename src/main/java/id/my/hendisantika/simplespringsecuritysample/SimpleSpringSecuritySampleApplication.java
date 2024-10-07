effd810ac714583a6129774058805c8b98700cd3
hendisantika <hendisantika@yahoo.co.id>
2024-10-08 05:56:42 +0700

    Create Spring Boot Project 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/simplespringsecuritysample/SimpleSpringSecuritySampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.simplespringsecuritysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringSecuritySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringSecuritySampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/simplespringsecuritysample/SimpleSpringSecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.simplespringsecuritysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringSecuritySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
