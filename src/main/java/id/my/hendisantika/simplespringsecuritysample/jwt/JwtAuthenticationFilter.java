ab96c711b81d39828d0b0a1a47c5076bc1bdf8f8
hendisantika <hendisantika@yahoo.co.id>
2024-10-08 06:10:36 +0700

    Add doFilterInternal in JwtAuthenticationFilter🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/simplespringsecuritysample/jwt/JwtAuthenticationFilter.java
+++ src/main/java/id/my/hendisantika/simplespringsecuritysample/jwt/JwtAuthenticationFilter.java
@@ -1,7 +1,16 @@
 package id.my.hendisantika.simplespringsecuritysample.jwt;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
@@ -19,4 +28,34 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        // get JWT token from http request
+        String token = getTokenFromRequest(request);
+
+        // validate token
+        if (StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
+
+            // get username from token
+            String username = jwtTokenProvider.getUsername(token);
+
+            // load the user associated with token
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
