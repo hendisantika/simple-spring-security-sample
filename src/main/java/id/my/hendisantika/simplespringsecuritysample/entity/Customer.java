f2e8844385257b860fb858b905ae6f5dae2e39ee
hendisantika <hendisantika@yahoo.co.id>
2024-10-08 05:59:54 +0700

    Add Customer Entity🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/simplespringsecuritysample/entity/Customer.java
@@ -0,0 +1,65 @@
+package id.my.hendisantika.simplespringsecuritysample.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : simple-spring-security-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/10/24
+ * Time: 05.59
+ * To change this template use File | Settings | File Templates.
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "customers")
+public class Customer extends User {
+
+    private Role role;
+
+    @Builder
+    public Customer(
+            Long id,
+            String email,
+            String password,
+            Role role) {
+        super(id, email, password, role);
+
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
