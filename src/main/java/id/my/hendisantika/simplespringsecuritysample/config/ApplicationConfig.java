1d2822631a7006c9abb6907de5fae55a6f4665e5
hendisantika <hendisantika@yahoo.co.id>
2024-10-08 06:08:17 +0700

    Add authenticationManager in ApplicationConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/simplespringsecuritysample/config/ApplicationConfig.java
+++ src/main/java/id/my/hendisantika/simplespringsecuritysample/config/ApplicationConfig.java
@@ -4,6 +4,8 @@ import id.my.hendisantika.simplespringsecuritysample.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -37,5 +39,10 @@ public class ApplicationConfig {
     @Bean
     BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
