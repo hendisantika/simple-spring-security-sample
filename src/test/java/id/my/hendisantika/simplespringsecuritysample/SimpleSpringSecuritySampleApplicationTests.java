bae6b350b6e0ef94d9db1a0de648d6bb781a6018
hendisantika <hendisantika@yahoo.co.id>
2024-10-08 06:21:28 +0700

    feat: Add Integration Test🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/simplespringsecuritysample/SimpleSpringSecuritySampleApplicationTests.java
+++ src/test/java/id/my/hendisantika/simplespringsecuritysample/SimpleSpringSecuritySampleApplicationTests.java
@@ -1,13 +1,45 @@
 package id.my.hendisantika.simplespringsecuritysample;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import id.my.hendisantika.simplespringsecuritysample.entity.LoginDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class SimpleSpringSecuritySampleApplicationTests {
 
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
-    void contextLoads() {
+    void works() throws Exception {
+        LoginDto loginDto = new LoginDto("user", "password");
+
+        this.mvc.perform(post("/accounts").contentType(MediaType.APPLICATION_JSON).content(this.objectMapper.writeValueAsBytes(loginDto)))
+                .andExpect(status().isOk());
+
+        MockHttpServletResponse response = this.mvc.perform(post("/auth/login").contentType(MediaType.APPLICATION_JSON).content(this.objectMapper.writeValueAsBytes(loginDto)))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+
+        String accessToken = JsonPath.read(response.getContentAsString(), "$.accessToken");
+        this.mvc.perform(get("/user/me").header("Authorization", "Bearer " + accessToken))
+                .andExpect(status().isOk());
     }
 
 }
