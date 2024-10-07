e314a018ba4507abf5dfa393d2f50f6b603b67bc
hendisantika <hendisantika@yahoo.co.id>
2024-10-08 06:10:50 +0700

    Add getTokenFromRequest in JwtAuthenticationFilter🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/simplespringsecuritysample/jwt/JwtAuthenticationFilter.java
+++ src/main/java/id/my/hendisantika/simplespringsecuritysample/jwt/JwtAuthenticationFilter.java
@@ -58,4 +58,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         filterChain.doFilter(request, response);
     }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+
+        return null;
+    }
 }
